0c93f6eeaf97bac5cb9e887e00b9b6fec846be47
Pouria Sarhadi <71611734+Psarhadi@users.noreply.github.com>
2025-07-15 17:49:43 +0100

    Add files via upload

---
+++ Chapter13-Formation Control/Ex1_no_obs/Run_Formation_no_obs.m
@@ -0,0 +1,20 @@
+clear all, close all, clc
+
+Sim_Time = 200;
+dt = 0.001;
+
+
+Xd = [150; 90];           % Goal
+Xob = [750 800; 40 65];     % Two obstacles (columns)
+
+u_min = 0.2;
+u_max = 2.5;
+
+u0 = 1;
+psi_r_sat = 10*pi/180; %10
+delta_max = 500*pi/180; %30
+
+%open('Formation_no_obs.slx')
+sim('Formation_no_obs.slx')
+
+Plotting_figures
